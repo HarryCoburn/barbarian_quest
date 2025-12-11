0dc13b638fd6dd35f9f6508085ba84d3c8a756d8
HarryCoburn <10048077+HarryCoburn@users.noreply.github.com>
2025-12-10 21:48:10 -0500

    Two more test in Map class

--- src/models/map.py
+++ src/models/map.py
@@ -1,6 +1,24 @@
 class Map:
+    HEX_OFFSETS = [
+        (0, -1),  # N
+        (1, -1),  # NE
+        (1, 0),  # SE
+        (0, 1),  # S
+        (-1, 0),  # SW
+        (-1, -1),  # NW
+    ]
+
     def __init__(self, map_data):
         self.data = map_data
 
-    def get_terrain(self, pos):        
+    def get_terrain(self, pos):
         return self.data[pos]["terrain"]
+
+    def is_in_bounds(self, pos):
+        x, y = pos
+        return x > 0 and x < 21 and y < 23
+
+    def get_valid_moves(self, pos):        
+        x, y = pos
+        valid = [(x + dx, y + dy) for dx, dy in Map.HEX_OFFSETS if self.is_in_bounds((x + dx, y + dy))]        
+        return valid    
--- src/tests/test_map.py
+++ src/tests/test_map.py
@@ -13,3 +13,25 @@ class Test_Map(unittest.TestCase):
         assert terrain == Terrain.FOREST
         
 
+    def test_map_boundaries(self):
+        in_bounds = self.map.is_in_bounds((2,2))
+        too_far_west = self.map.is_in_bounds((0,10))
+        too_far_east = self.map.is_in_bounds((21, 21))
+        too_far_south = self.map.is_in_bounds((5,24))
+        gone_home = self.map.is_in_bounds((10,0))
+
+        assert in_bounds is True
+        assert too_far_east is False
+        assert too_far_west is False
+        assert too_far_south is False
+        assert gone_home is True
+
+    def test_get_valid_moves(self):
+        middle_of_map = self.map.get_valid_moves((5,5))
+        edge_of_map = self.map.get_valid_moves((1,5))
+        corner_of_map = self.map.get_valid_moves((20, 23))
+
+        assert len(middle_of_map) == 6
+        assert len(edge_of_map) == 4
+        assert len(corner_of_map) == 2
+
