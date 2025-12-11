24e92837cbb849e9af741dd69225debcf86be44a
HarryCoburn <10048077+HarryCoburn@users.noreply.github.com>
2025-12-11 10:12:29 -0500

    get_valid_moves test

--- src/controllers/game_controller.py
+++ src/controllers/game_controller.py
@@ -1,6 +1,6 @@
 class GameController:
     def __init__(self, game_map, game_state):
-        self.map = game_map
+        self.game_map = game_map
         self.game_state = game_state
 
     def move_player(self, pos):
--- src/tests/test_game_controller.py
+++ src/tests/test_game_controller.py
@@ -11,3 +11,10 @@ class TestGameController(unittest.TestCase):
     def test_move_player(self):
         self.game_controller.move_player((5,5))
         assert self.game_controller.game_state.current_pos == (5,5)
+
+    def get_valid_moves(self):
+        self.game_controller.move_player((5,5)) # Force a starting position, since starting pos can be random
+        valid_moves = self.game_controller.game_map.get_valid_moves(self.game_controller.game_state.current_pos)
+
+        valid_move_set = {(5,4), (6,4), (6,5), (5,6), (4,5), (4,4)}
+        assert valid_move_set.issubset(valid_moves)
