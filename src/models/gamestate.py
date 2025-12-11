8c723d5cd5dfbfce924cd8cdab428c21deea92d8
HarryCoburn <10048077+HarryCoburn@users.noreply.github.com>
2025-12-11 10:06:42 -0500

    Changed typing

--- src/models/gamestate.py
+++ src/models/gamestate.py
@@ -2,5 +2,5 @@ from dataclasses import dataclass
 
 @dataclass
 class GameState:
-    current_pos = (0,0) # This will be set to a random position from a set once we have more code
-    gold = 0
+    current_pos: tuple[int,int] = (0,0) # This will be set to a random position from a set once we have more code
+    gold: int = 0
